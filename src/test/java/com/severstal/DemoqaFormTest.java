5336ed1d5bcdb73b8979de967ec3c5d43e921307
da.vasilyev <dmitry.vasilyew@gmail.com>
2022-01-24 22:05:20 +0300

    fixed test demoqa

--- src/test/java/com/severstal/DemoqaFormTest.java
+++ src/test/java/com/severstal/DemoqaFormTest.java
@@ -1,8 +1,6 @@
 package com.severstal;
 
-import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
-import com.severstal.helpers.Attachments;
 import com.severstal.pages.PracticeFormPage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,15 +10,12 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
-public class DemoqaFormTest {
+public class DemoqaFormTest extends TestBase{
     private static final Faker faker = new Faker();
     private static final TestData testData = new TestData();
 
     @BeforeAll
     static void BeforeAll() {
-        Configuration.browserSize = "1920x1280";
-        open("https://demoqa.com/automation-practice-form");
-
         testData
                 .setFirstName(faker.name().firstName())
                 .setLastName(faker.name().lastName())
@@ -28,6 +23,8 @@ public class DemoqaFormTest {
                 .setUserNumber(faker.phoneNumber().subscriberNumber(10))
                 .setCurrentAddress(faker.address().fullAddress())
                 .setDateOfBirth(faker);
+
+        open("/automation-practice-form");
     }
 
     @Test
@@ -40,10 +37,6 @@ public class DemoqaFormTest {
 
     @AfterAll
     static void AfterAll() {
-        Attachments.screenshotAs("Last screenshot");
-        Attachments.pageSource();
-        Attachments.browserConsoleLogs();
-        Attachments.addVideo();
         closeWebDriver();
     }
 }
---
+++ src/test/java/com/severstal/TestBase.java
@@ -0,0 +1,34 @@
+package com.severstal;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.severstal.helpers.Attachments;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+    }
+}
