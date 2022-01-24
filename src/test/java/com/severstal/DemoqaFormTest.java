27775f4eb7e2ccb655d2ab5f3d8ec61dd42ffcb0
da.vasilyev <dmitry.vasilyew@gmail.com>
2022-01-24 21:05:39 +0300

    fix test demoqa

--- src/test/java/com/severstal/DemoqaFormTest.java
+++ src/test/java/com/severstal/DemoqaFormTest.java
@@ -1,19 +1,23 @@
 package com.severstal;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.github.javafaker.Faker;
 import com.severstal.pages.PracticeFormPage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DemoqaFormTest {
     private static final Faker faker = new Faker();
     private static final TestData testData = new TestData();
 
-    @BeforeEach
-    void beforeEach() {
+    @BeforeAll
+    static void BeforeAll() {
         Configuration.browserSize = "1920x1280";
         open("https://demoqa.com/automation-practice-form");
 
@@ -31,5 +35,10 @@ public class DemoqaFormTest {
         new PracticeFormPage()
                 .setStudentRegistrationForm(testData)
                 .checkStudentRegistrationForm(testData);
+    }
+
+    @AfterAll
+    static void AfterAll() {
+        closeWebDriver();
     }
 }
